29529624eb379208d02be3a12b92caa85d2f3be6
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-06-03 10:52:28 +0200

    nächster build versuch via commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello DevOps Course :-)))');
+res.send('Hello DevOps Course - This Update is created with a GitHub Action! Incredible not?');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
